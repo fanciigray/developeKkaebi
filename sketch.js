0b86553741564d9914ad0cf20c422038446ee57c
fanciigray <fanciigray@gmail.com>
2020-12-09 23:33:17 +0900

    멋똥꾸(1)
    게임 작업 중
    드래그되는 스프라이트 만들기 왤케 어렵냐~!^^

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
-var 
+var redThing;
+var shape;
 
 function setup() {
     var canvas = createCanvas(960, 720);
     canvas.parent('game-kkeabimarket');
 
+    redThing = new Draggable();
 }
 
 function draw() {
-    background(0);
+    background(248, 240, 222);
+    redThing.update();
+    redThing.over();
+    redThing.show();
+
+}
+
+function mousePressed() {
+    redThing.pressed();
+}
+
+function mouseReleased() {
+    redThing.released();
 }
