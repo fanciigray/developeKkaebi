2f322058b2af129831d741d28f9f41471b684c23
HYPark244 <qkrgkdud9080@gmail.com>
2020-12-10 01:04:52 +0900

    카드 뒤집기까지 완료
    판정 로직만 남음

--- game-testing/sketch.js
+++ game-testing/sketch.js
@@ -1,19 +1,17 @@
 //variables setting
-var bear, cat, cir, hexa, penta, squa, star, tri;
 const numRows = 4
 const numColumns = 4
-var faceUpImages = [] ;
-var imagesDeck = [String] ;
-var ghost;
+var faceUpImages = ['../src/bear.png', '../src/cat.png', '../src/circle.png', '../src/hexagon.png',
+'../src/pentagon.png', '../src/square.png', '../src/star.png', '../src/triangle.png'];
+var imagesDeck = [] ;
 
+//setup
 function setup() {
     createCanvas(720, 560);
     background(155);
 
     createTiles();
     
-
-
 }
 
 function draw() {
@@ -22,48 +20,62 @@ function draw() {
 
 }
 
-//카드 생성(뒤집은 상태)
+//카드 생성
 function createTiles() {
+    var count = 0;
+    var countFaceUp = 0;
 
+    //이미지 리스트 불러오기
     createImagesDeck(faceUpImages);
     
+    //카드 배열 및 생성
     for(var i=0; i<numRows; i++) {
-        for (var j =0 ; j<numColumns; j++) {
+        for (var j = 0 ; j<numColumns; j++) {
         var back = createSprite(i * 95 + 60, j * 115 + 70);
-        back.addAnimation('change', '../src/background.png', imagesDeck[i*j]);
+        back.addAnimation('change', '../src/background.png', imagesDeck[count]);
         back.animation.playing = false;
+        count ++;
+        
 
         back.onMousePressed = function() {
-            this.animation.nextFrame();
+            
+            //카드 최대 2개 선택
+            if (countFaceUp == 2) {
+                this.animation.playing = false;
+
+                //여기서 판정 진행
+
+            }
+            else {
+                this.animation.nextFrame();
+                
+                //카드 1개만 선택 시 자유롭게 뒤집기
+                var flip = this.animation.getFrame();
+                if (flip == 1) {
+                countFaceUp ++;}
+                else {
+                        countFaceUp --;
+                }
+               
+            }
          }
+        
         }
-    }
-    }
-
-function loadFaceUpImages() {
-    faceUpImages = ['../src/bear.png', '../src/cat.png', '../src/circle.png', '../src/hexagon.png',
-    '../src/pentagon.png', '../src/square.png', '../src/star.png', '../src/triangle.png']
       }
+    }
 
-//짝 생성되도록 리스트 생성하기
+
+//이미지 짝 리스트 생성
       function createImagesDeck(images) {
     for (let i = 0; i < faceUpImages.length; i++) {
-          imagesDeck.push(images[i])
-          imagesDeck.push(images[i])
+          imagesDeck.push(images[i]);
+          imagesDeck.push(images[i]);
         }
       
     imagesDeck.sort(function() {
-          return 0.5 - random()
+          return 0.5 - random();
           //난수 지정해서 랜덤하게 섞이도록 함 (-1.5~1.5이내 범위)
         })
+
+        console.log(faceUpImages[0]);
     }
-
-        /*ghost = createSprite(100, 100);
-    ghost.addAnimation('change', '../src/background.png', '../src/bear.png');
-    ghost.animation.playing = false;
-
-
-    ghost.onMousePressed = function() {
-        this.animation.nextFrame();
-    }*/
-
