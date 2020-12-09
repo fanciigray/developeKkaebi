b3289c2ce6e557f3812d41d1986dbb2563873dc3
HYPark244 <qkrgkdud9080@gmail.com>
2020-12-09 13:59:33 +0900

    animation problem
    animation 변수 오류난다고 함

--- game-testing/sketch.js
+++ game-testing/sketch.js
@@ -1,7 +1,28 @@
-function setup() {
+//animation setting
+var bear, cat, cir, hexa, penta, sq, star, tri;
 
+function preload() {
+    bear = loadAnimation('src/background.png', 'src/bear.png')
+    bear.looping = false;
+
+}
+
+function setup() {
+    createCanvas(720, 560);
+    background(365);
 }
 
 function draw() {
+
+    animation(bear, 300, 150);
     
 }
+
+function mousePressed() {
+  
+    //move ahead one frame
+    bear.nextFrame();
+    //glitch.previousFrame();
+
+  }
+
