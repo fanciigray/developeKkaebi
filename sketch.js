488b7e884a2261097a843ef11d1832d8da08c34d
fanciigray <fanciigray@gmail.com>
2020-12-16 15:56:51 +0900

    map을 시도중(1)

--- sketch.js
+++ sketch.js
@@ -1,25 +1,11 @@
-var redThing;
-var shape;
+
 
 function setup() {
-    var canvas = createCanvas(960, 720);
-    canvas.parent('game-kkeabimarket');
+    var map = createCanvas(960, 720);
+    map.parent('game-kkeabimarket');
 
-    redThing = new Draggable();
 }
 
 function draw() {
     background(248, 240, 222);
-    redThing.update();
-    redThing.over();
-    redThing.show();
-
-}
-
-function mousePressed() {
-    redThing.pressed();
-}
-
-function mouseReleased() {
-    redThing.released();
 }
